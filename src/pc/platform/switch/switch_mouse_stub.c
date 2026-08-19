c249f6b3f2fc67e8d6c018e78cfd36b32a477c7f
Antoine Boucher <antoine.bou13@gmail.com>
2026-08-19 01:13:36 -0400

    switch: stub desktop mouse input

---
+++ src/pc/platform/switch/switch_mouse_stub.c
@@ -0,0 +1,25 @@
+#ifdef __SWITCH__
+
+#include "pc/controller/controller_mouse.h"
+
+bool mouse_init_ok = false;
+u32 mouse_buttons = 0;
+f32 mouse_x = 0;
+f32 mouse_y = 0;
+u32 mouse_window_buttons = 0;
+u32 mouse_window_buttons_pressed = 0;
+u32 mouse_window_buttons_released = 0;
+f32 mouse_window_x = 0;
+f32 mouse_window_y = 0;
+u32 mouse_scroll_timestamp = 0;
+f32 mouse_scroll_x = 0;
+f32 mouse_scroll_y = 0;
+bool mouse_relative_enabled = false;
+
+void controller_mouse_read_window(void) {}
+void controller_mouse_read_relative(void) {}
+void controller_mouse_enter_relative(void) { mouse_relative_enabled = true; }
+void controller_mouse_leave_relative(void) { mouse_relative_enabled = false; }
+void mouse_on_scroll(float x, float y) { (void)x; (void)y; }
+
+#endif
