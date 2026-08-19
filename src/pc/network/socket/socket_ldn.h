635e596c72fd302f2edd051b625dadc3ee03c459
Antoine Boucher <antoine.bou13@gmail.com>
2026-08-19 00:41:21 -0400

    switch: expose LDN scan cleanup

--- src/pc/network/socket/socket_ldn.h
+++ src/pc/network/socket/socket_ldn.h
@@ -12,6 +12,7 @@ void network_ldn_select(void);
 /* Local-wireless lobby discovery helpers for the Switch UI. */
 bool network_ldn_refresh_scan(void);
 bool network_ldn_connect_to_index(int index);
+void network_ldn_cancel_scan(void);
 int network_ldn_network_count(void);
 const char *network_ldn_network_name(int index);
 int network_ldn_network_player_count(int index);
