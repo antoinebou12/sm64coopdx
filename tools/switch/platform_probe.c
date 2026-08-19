7430aaf4e41ea6309c7bd2f7d5b28e185c451d13
Antoine Boucher <antoine.bou13@gmail.com>
2026-08-19 00:54:13 -0400

    switch: exercise big-stack path in platform probe

--- tools/switch/platform_probe.c
+++ tools/switch/platform_probe.c
@@ -3,7 +3,7 @@
 
 #include "pc/platform/switch/switch_platform.h"
 
-int main(int argc, char **argv) {
+static int platform_probe_main(int argc, char **argv) {
     (void)argc;
     (void)argv;
 
@@ -21,8 +21,13 @@ int main(int argc, char **argv) {
     printf("suspended=%d\n", state.suspended ? 1 : 0);
     printf("hos_version=0x%08x\n", state.hos_version);
     printf("data_root=%s\n", switch_platform_data_root());
+    printf("game_stack_bytes=%u\n", (unsigned int)SWITCH_PLATFORM_GAME_STACK_SIZE);
 
     switch_platform_shutdown(&state);
     consoleExit(NULL);
     return 0;
+}
+
+int main(int argc, char **argv) {
+    return switch_platform_run_main_on_game_thread(platform_probe_main, argc, argv);
 }
