6f6e8242d2894319533078b71ebc7cf70b0d9447
Antoine Boucher <antoine.bou13@gmail.com>
2026-08-20 14:19:33 -0400

    debug(nx): expose ROM asset failure snapshot state

--- src/pc/platform/switch/switch_rom_asset_trace.h
+++ src/pc/platform/switch/switch_rom_asset_trace.h
@@ -7,6 +7,8 @@ extern "C" {
 #endif
 
 void switch_rom_asset_trace_printf(const char *fmt, ...);
+unsigned int switch_rom_asset_trace_failure_count(void);
+void switch_rom_asset_trace_exception_snapshot(void);
 
 #ifdef __cplusplus
 }
