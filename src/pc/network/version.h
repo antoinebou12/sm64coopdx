37ff3ec8354917fc3aa6778e3d195f7aa25ee0e4
Antoine Boucher <antoine.bou13@gmail.com>
2026-08-22 11:32:17 -0400

    fix(nx): align all version constants with v1.5.1

--- src/pc/network/version.h
+++ src/pc/network/version.h
@@ -5,8 +5,8 @@
 
 // internal version
 #define VERSION_TEXT "v"
-#define VERSION_NUMBER 43
-#define MINOR_VERSION_NUMBER 0
+#define VERSION_NUMBER 42
+#define MINOR_VERSION_NUMBER 1
 
 #define VERSION_OFFSET 37 // difference from old versioning system
 
