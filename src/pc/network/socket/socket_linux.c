3f8dc81eb6c42f1e41b42e588eb66231e74bf806
Antoine Boucher <antoine.bou13@gmail.com>
2026-08-25 16:10:14 -0400

    fix(nx): open native IPv4 UDP socket for direct connect

--- src/pc/network/socket/socket_linux.c
+++ src/pc/network/socket/socket_linux.c
@@ -4,29 +4,35 @@
 #include "pc/debuglog.h"
 
 SOCKET socket_initialize(void) {
-    // initialize socket
+#ifdef __SWITCH__
+    SOCKET sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+#else
     SOCKET sock = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+#endif
     if (sock == INVALID_SOCKET) {
         LOG_ERROR("socket failed with error %d", SOCKET_LAST_ERROR);
         return INVALID_SOCKET;
     }
 
-    // set non-blocking mode
     int rc = fcntl(sock, F_SETFL, fcntl(sock, F_GETFL, 0) | O_NONBLOCK);
     if (rc == (int)INVALID_SOCKET) {
         LOG_ERROR("fcntl failed with error: %d", rc);
+        closesocket(sock);
         return INVALID_SOCKET;
     }
 
-    // Set socket to dual-stack
+#ifndef __SWITCH__
     int v6only = 0;
     if (setsockopt(sock, IPPROTO_IPV6, IPV6_V6ONLY, (void *)&v6only, sizeof(v6only)) < 0) {
         LOG_ERROR("setsockopt(IPV6_V6ONLY) failed.");
+        closesocket(sock);
         return INVALID_SOCKET;
-    };
+    }
+#else
+    LOG_INFO("Switch Direct: IPv4 UDP socket initialized");
+#endif
 
     LOG_INFO("socket initialized.");
-
     return sock;
 }
 
