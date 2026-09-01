e50b158adade71c1fe350dc0b23386ab95feac8e
Antoine Boucher <antoine.bou13@gmail.com>
2026-09-01 08:31:35 -0400

    new3ds: track native SOC runtime state

--- src/pc/platform/new3ds/new3ds_runtime.h
+++ src/pc/platform/new3ds/new3ds_runtime.h
@@ -18,7 +18,9 @@ typedef struct New3dsRuntimeState {
     bool initialized;
     bool is_new_3ds;
     bool speedup_enabled;
+    bool soc_initialized;
     bool exit_requested;
+    void *soc_buffer;
     uint64_t start_ms;
     uint64_t frame_index;
     New3dsInputState input;
@@ -29,6 +31,7 @@ void new3ds_runtime_shutdown(New3dsRuntimeState *state);
 bool new3ds_runtime_poll(New3dsRuntimeState *state);
 void new3ds_runtime_request_exit(New3dsRuntimeState *state);
 New3dsRuntimeState *new3ds_runtime_active(void);
+bool new3ds_runtime_network_available(void);
 uint64_t new3ds_runtime_time_ms(void);
 double new3ds_runtime_time_seconds(void);
 void new3ds_runtime_sleep_ms(uint32_t ms);
