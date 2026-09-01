628067f628c0cb70710f1167050dd0382c49c313
Antoine Boucher <antoine.bou13@gmail.com>
2026-09-01 19:15:07 -0400

    new3ds: unify Ultra64 and libctru fixed-width types

--- include/PR/ultratypes.h
+++ include/PR/ultratypes.h
@@ -8,6 +8,16 @@
 #define TRUE 1
 #define FALSE 0
 
+#if defined(__3DS__)
+/*
+ * libctru and the historical Ultra64 headers both own the s8/u8/... names.
+ * On devkitARM some fixed-width typedefs do not use the same underlying C
+ * type as the legacy definitions (notably s32/u32), so redefining them in one
+ * translation unit is an error. For the native 3DS target, make libctru the
+ * single source of truth for integer/volatile aliases.
+ */
+#include <3ds/types.h>
+#else
 typedef signed char            s8;
 typedef unsigned char          u8;
 typedef signed short int       s16;
@@ -25,6 +35,7 @@ typedef volatile s8   vs8;
 typedef volatile s16 vs16;
 typedef volatile s32 vs32;
 typedef volatile s64 vs64;
+#endif
 
 typedef float  f32;
 typedef double f64;
@@ -33,7 +44,7 @@ typedef double f64;
 #include <stdint.h>
 #include <sys/types.h>
 
-#if defined(__MINGW32__) 
+#if defined(__MINGW32__)
 #include <_mingw.h>
 #if !defined(__MINGW64_VERSION_MAJOR)
 typedef long ssize_t;
